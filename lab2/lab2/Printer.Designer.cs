8f0d786b93eb100288caeae311dd700b345a9ef0
mateuszgurski444 <mateuszgurski444@gmail.com>
2019-10-28 22:25:30 +0100

    prawie gotowe

---
+++ lab2/lab2/Printer.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace lab2
+{
+    partial class Printer
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Drukuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 246);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Czcionka";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Times New Roman",
+            "Courier",
+            "Arial"});
+            this.comboBox1.Location = new System.Drawing.Point(280, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rozmiar czcionki";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40"});
+            this.comboBox2.Location = new System.Drawing.Point(280, 95);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tekst";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kolor czcionki";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Czerwony",
+            "Zielony",
+            "Niebieski",
+            "Czarny"});
+            this.comboBox3.Location = new System.Drawing.Point(280, 147);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(120, 21);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(285, 182);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Pogrubienie";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(285, 205);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(66, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Kursywa";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(285, 226);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(87, 17);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "Podkreślenie";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Printer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 304);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Printer";
+            this.Text = "Printer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ lab2/lab2/Printer.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+using System.Drawing.Printing;
+
+namespace lab2
+{
+    public partial class Printer : Form
+    {
+        public Printer()
+        {
+            InitializeComponent();
+        }
+        public string Font_type()
+        {
+            string ftype = " ";
+
+            if (comboBox1.SelectedItem != null)
+            {
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "Arial":
+                        ftype = "(s218T";
+                        break;
+                    case "Courier":
+                        ftype = "(s3T";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return ftype;
+        }
+
+        public string Font_size()
+        {
+            string fsize = "10";
+
+            if (comboBox2.SelectedItem != null)
+            {
+                return comboBox2.SelectedItem.ToString();
+            }
+
+            return fsize;
+        }
+
+        public string Font_color()
+        {
+            string fcolor = "*v0S";
+
+            if (comboBox3.SelectedItem != null)
+            {
+                switch (comboBox3.SelectedItem.ToString())
+                {
+                    case "Czerwony":
+                        fcolor = "*v1S";
+                        break;
+                    case "Zielony":
+                        fcolor = "*v2S";
+                        break;
+                    case "Niebieski":
+                        fcolor = "*v12S";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return fcolor;
+        }
+
+        public bool Print()
+        {
+            string path = @".\file";
+            FileStream file = File.Create(path);
+            StreamWriter writer = new StreamWriter(file);
+
+            string text = textBox1.Text;
+               
+            writer.WriteLine("\x1B" + "E");
+            writer.WriteLine("\x1B" + "&s0C");
+            writer.WriteLine("\x1B" + "&10O"); 
+            writer.WriteLine("\x1B" + "*o0M"); 
+            writer.WriteLine("\x1B" + "&l26A");
+
+          
+            //chosing the font type
+            writer.WriteLine("\x1B" + Font_type());
+
+            //chosing the font size
+
+            writer.WriteLine("\x1B" + "(s" + Font_size() + "V");
+
+            //chosing the font color
+            writer.WriteLine("\x1B" + "*r3U");
+            writer.WriteLine("\x1B" + Font_color());
+
+            //bold
+            if (checkBox1.Checked)
+            {
+                writer.WriteLine("\x1B" + "(s3B");
+            }
+            //italic
+            if (checkBox2.Checked)
+            {
+                writer.WriteLine("\x1B" + "(s1S");
+            }
+            //underline
+            if (checkBox3.Checked)
+            {
+                writer.WriteLine("\x1B" + "&d0D");
+            }
+
+
+            if (text.Equals(""))
+                return false;
+
+            writer.WriteLine(text);
+            writer.WriteLine("\x1B" + "E");
+            writer.Close();
+            File.Copy(path, "LPT3");
+          
+            return true;
+        }
+
+        public bool PrintRaster()
+        {
+            string path = @".\file";
+            FileStream file = File.Create(path);
+            StreamWriter writer = new StreamWriter(file);
+
+            writer.WriteLine("\x1B" + "E");
+
+            //Simple color mode, -3 = 3 plates, CMY palette. 3 = 3 plates, RGB palette.
+            writer.WriteLine("\x1B" + "*r-3U");
+
+            //Start raster 
+            writer.WriteLine("\x1B" + "r0A");
+
+            //Compression method. 1 = Run-length encoding. Requies byte pairs.
+            writer.WriteLine("\x1B" + "b1M");
+
+
+
+
+
+
+            writer.WriteLine("\x1B" + "E");
+            writer.Close();
+            File.Copy(path, "LPT3");
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Print())
+                MessageBox.Show("Nie udalo sie");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
